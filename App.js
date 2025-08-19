c788be57c301dd24b6e166352d889c461fe6738a
Mluleki <melokuhlemluleki02@gmail.com>
2025-08-19 17:07:30 +0200

    add username storage with localStorage and form

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import NumberWork from "./numberWork.js";
 import FormControl from "./FormControl.js";
+
+let getName = document.getElementById("getName");
+let fillform = document.getElementById("HelloSection");
+let username = document.getElementById("username");
 if (localStorage.getItem("username")) {
   alert("Username");
+  fillform.style.display="block";
+  username.textContent = localStorage.getItem("username")
+  getName.style.display= "none"
 } else {
   alert("No username found in localStorage.");
+  getName.style.display = "block"
 }
+ let saveBtn = document.getElementById("saveBtn");
+ saveBtn.addEventListener("click", setname)
+  function setname() {
+    const input = document.getElementById("inputName");
+    let name = input.value;
+   localStorage.setItem("username", name)
+   fillform.style.display="block"
+   getName.style.display="none"
+    username.textContent = localStorage.getItem("username");
+  }
 
 window.start = () => {
     let x = 5;
